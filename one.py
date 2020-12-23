a3cc77623fbc4f619de16f8c6d0aee8367e4d097
ivy liu <ivy.liu.liuwanying@outlook.com>
2020-12-23 23:01:33 +0800

    困

---
+++ one.py
@@ -0,0 +1,14 @@
+'''
+Description: 学习尝试及重装电脑环境重配，不用回顾再看这个
+Version: 
+Autor: ivy-liu
+Date: 2020-12-23 22:49:45
+LastEditors: ivy-liu still me
+LastEditTime: 2020-12-23 23:00:33
+'''
+
+import numpy as np
+import pandas as pd
+
+s=pd.Series([-1.555714,-0.4441235545],index=['a','b'])
+print (s)
